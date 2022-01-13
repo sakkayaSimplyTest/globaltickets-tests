384243f1ad160d34b9c7c30ffad4efdbc6d93f7c
sakkaya <sakkaya@simlytest.de>
2022-01-13 10:27:43 +0100

    first cypress test added

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -4,17 +4,13 @@ describe('Navigation', () => {
     it('should have a list events link that navigates to the list events view', () => {
         //navigate to the homepage
        // cy.visit('http://localhost:8080');
-
        cy.visit('/');
-
         // check that the "List Events" link is there
-
-
+        expect(cy.get('[href="/list-events"]')).to.exist;
         //click on the list " List Events" link
-
-
+        cy.get('a[href="/list-events"]').click
         // verify a user has been redirected to the "List Events" page
-
-
+      //  cy.get('h2').contains('Upcoming events');
+        expect(cy.get('h2').contains('Upcoming events')).to.exist;
     });
 });

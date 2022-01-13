d0234cd8fc5f60a6d668cbf47e31c1246c2383d4
sakkaya <sakkaya@simlytest.de>
2022-01-13 09:18:43 +0100

    first commit

---
+++ cypress/integration/nav.spec.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress" />
+
+describe('Navigation', () => {
+    it('should have a list events link that navigates to the list events view', () => {
+        //navigate to the homepage
+       // cy.visit('http://localhost:8080');
+
+       cy.visit('/');
+
+        // check that the "List Events" link is there
+
+
+        //click on the list " List Events" link
+
+
+        // verify a user has been redirected to the "List Events" page
+
+
+    });
+});

44be931f3f7c9c3cb0b7eff35afc76c87a83f4dc
sakkaya <sakkaya@simlytest.de>
2022-01-13 12:22:35 +0100

    test nav refactor

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -1,14 +1,17 @@
 /// <reference types = "cypress" />
 
+$listEvent = '[href="/list-events"]';
+function anchor(arg0) { return 'a' + (arg0); }
+
 describe('Navigation', () => {
     it('should have a list events link that navigates to the list events view', () => {
         //navigate to the homepage
        // cy.visit('http://localhost:8080');
        cy.visit('/');
         // check that the "List Events" link is there
-        expect(cy.get('[href="/list-events"]')).to.exist;
+        expect(cy.get($listEvent)).to.exist;
         //click on the list " List Events" link
-        cy.get('a[href="/list-events"]').click
+        cy.get(anchor($listEvent)).click
         // verify a user has been redirected to the "List Events" page
       //  cy.get('h2').contains('Upcoming events');
         expect(cy.get('h2').contains('Upcoming events')).to.exist;
